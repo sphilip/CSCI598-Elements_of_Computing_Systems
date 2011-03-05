93c23faa5be37eab5674d1d451f8406075910c3f
Shanley Philip <sphilip@mymail.mines.edu>
2011-03-04 17:13:31 -0700

    VM now works on Stack Test

--- project07/CodeWriter.rb
+++ project07/CodeWriter.rb
@@ -1,17 +1,17 @@
 # tranlsation VM commands into Hack assembly code
 class CodeWriter
-  
+$i = 0
   # open output file
   def initialize
   end
-  
+
   # inform code writer that translation of new VM file is started
   # input: string (file name)
   # output: none
   def  setFileName(path)
   end
-  
-  # write assembly code 
+
+  # write assembly code
   # input: string (the command)
   # otuput: none
   def writeArithmetic(command)
@@ -23,8 +23,8 @@ AM=M-1
 D=M
 @SP
 A=M-1
-M=M+D\n\n"
-      
+M=M+D \n\n"
+
     elsif command.downcase == "sub" then
       output = "// subtract
 @SP
@@ -32,16 +32,132 @@ AM=M-1
 D=M
 @SP
 A=M-1
-M=M-D\n\n"
-      
+M=M-D \n\n"
+
+    elsif command.downcase == "eq" then
+      output = "// equals?
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+A=M
+D=D-A
+@TRUE_#{$i}
+D;JEQ
+(FALSE_#{$i})
+@SP
+A=M-1
+M=0
+@EXIT_#{$i}
+0;JMP
+
+(TRUE_#{$i})
+@SP
+A=M-1
+M=-1
+
+(EXIT_#{$i})\n\n"
+      $i=$i+1
+
+    elsif command.downcase == "lt" then
+      output = "// less than?
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+A=M
+D=A-D
+@TRUE_#{$i}
+D;JLT
+(FALSE_#{$i})
+@SP
+A=M-1
+M=0
+@EXIT_#{$i}
+0;JMP
+
+(TRUE_#{$i})
+@SP
+A=M-1
+M=-1
+
+(EXIT_#{$i})\n\n"
+      $i=$i+1
+
+
+  elsif command.downcase == "gt" then
+    output = "// greater than?
+@SP
+AM=M-1
+D=M
+@SP
+A=M-1
+A=M
+D=A-D
+@TRUE_#{$i}
+D;JGT
+(FALSE_#{$i})
+@SP
+A=M-1
+M=0
+@EXIT_#{$i}
+0;JMP
+
+(TRUE_#{$i})
+@SP
+A=M-1
+M=-1
+
+(EXIT_#{$i}) \n\n"
+      $i=$i+1
+
+    elsif command.downcase == "neg" then
+      output = "// negate value at SP-1
+@SP
+AM=M-1
+D=-M
+M=D
+@SP
+M=M+1 \n\n"
+
+    elsif command.downcase == "and" then
+      output = "// bitwise and
+@SP
+AM=M-1
+D=M
+@SP
+AM=M-1
+A=M
+D=A&D
+@SP
+A=M
+M=D
+@SP
+M=M+1 \n\n"
+
+    elsif command.downcase == "or" then
+      output = "// bitwise or
+@SP
+AM=M-1
+D=M
+@SP
+AM=M-1
+A=M
+D=A|D 
+@SP
+A=M
+M=D 
+@SP
+M=M+1 \n\n"
+
     end
-    
     return output
   end
-  
-  
-  
-  # write assembly code that's the tranlsation of a 
+
+
+  # write assembly code that's the tranlsation of a
   # given command (for C_PUSH/ C_POP)
   # input: command (C_PUSH/C_POP), string (segment), int (index)
   # output: none
@@ -57,7 +173,7 @@ A=M
 M=D
 @SP
 M=M+1\n\n"
-      
+
     # command is pop
     else
       output == "//pop #{segment} #{index}
@@ -69,10 +185,10 @@ M=D
 @SP
 M=M-1\n\n"
     end
-    
+
     return output
   end
-  
+
   # close output file
   def close
   end
--- project07/TranslatorDriver.rb
+++ project07/TranslatorDriver.rb
@@ -5,12 +5,12 @@ require 'CodeWriter'
 
 ARGV.each do |param|
   param = param.strip
-  
+
   # seems like user knows what's going on
-  if param.size > 0
+  if !param.empty?
     parse = Parser.new(param)
-    
-  # user is completely clueless
+
+  # user is an abject moron.  The only prescription is more cowbell
   else
     print "Nothing specified.\nUsage: ./TranslatorDriver <files or directories>\n\n"
   end
