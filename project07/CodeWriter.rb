89b31798cb439bd3b68d94d703348872eb7c0ea2
Shanley Philip <sphilip@mymail.mines.edu>
2011-03-07 00:07:59 -0700

    VM now works on the Basic Test

--- project07/CodeWriter.rb
+++ project07/CodeWriter.rb
@@ -9,18 +9,21 @@ $label_index=0
   def initialize(path)
 
     @output = File.open(path,"w")
+
     @segment_hash = {
       "push constant" => "./templates/push-constant.erb",
       "push local" => "./templates/push-lcl.erb",
       "push argument" => "./templates/push-arg.erb",
       "push this" => "./templates/push-this.erb",
       "push that" => "./templates/push-that.erb",
+      "push temp" => "./templates/push-temp.erb",
 
       "pop constant" => "./templates/pop-constant.erb",
       "pop local" => "./templates/pop-lcl.erb",
       "pop argument" => "./templates/pop-arg.erb",
       "pop this" => "./templates/pop-this.erb",
-      "pop that" => "./templates/pop-that.erb"
+      "pop that" => "./templates/pop-that.erb",
+      "pop temp" => "./templates/pop-temp.erb"
     }
 
     @arith_hash = {
@@ -32,6 +35,7 @@ $label_index=0
       "and" => "./templates/and.erb",
       "or" => "./templates/or.erb"
     }
+
   end
 
   # inform code writer that translation of new VM file is started
@@ -40,203 +44,43 @@ $label_index=0
   def  setFileName(path)
   end
 
-  def writeArith(file)
-    infile = File.open(file,'r')
-    erb = ERB.new(infile.read)
-    @output.write erb.result(binding)
-  end
 
   # write assembly code
   # input: string (the command)
   # output: none
   def writeArithmetic(command)
-    if @arith_hash.has_key?(command)
-      writeArith(@arith_hash[command])
+
+    if @arith_hash.has_key?(command) then
+      File.open(@arith_hash[command],'r') do |infile|
+	erb = ERB.new(infile.read)
+	@output.write erb.result(binding)
+
+      end
+
     else puts "#{command} not supported. \n\n"
     end
 
-#     output = ""
-#     if command.downcase == "add" then
-#       output = "// add
-# @SP
-# AM=M-1
-# D=M
-# @SP
-# A=M-1
-# M=M+D \n\n"
-#     writeArith(@arith_hash[command.downcase]
-#     elsif command.downcase == "sub" then
-#       output = "// subtract
-# @SP
-# AM=M-1
-# D=M
-# @SP
-# A=M-1
-# M=M-D \n\n"
-#
-#     elsif command.downcase == "eq" then
-#       output = "// equals?
-# @SP
-# AM=M-1
-# D=M
-# @SP
-# A=M-1
-# A=M
-# D=D-A
-# @TRUE_#{$label_index}
-# D;JEQ
-#
-# (FALSE_#{$label_index})
-# @SP
-# A=M-1
-# M=0
-# @EXIT_#{$label_index}
-# 0;JMP
-#
-# (TRUE_#{$label_index})
-# @SP
-# A=M-1
-# M=-1
-#
-# (EXIT_#{$label_index})\n\n"
-#       $label_index=$label_index+1
-#
-#     elsif command.downcase == "lt" then
-#       output = "// less than?
-# @SP
-# AM=M-1
-# D=M
-# @SP
-# A=M-1
-# A=M
-# D=A-D
-# @TRUE_#{$label_index}
-# D;JLT
-# (FALSE_#{$label_index})
-# @SP
-# A=M-1
-# M=0
-# @EXIT_#{$label_index}
-# 0;JMP
-#
-# (TRUE_#{$label_index})
-# @SP
-# A=M-1
-# M=-1
-#
-# (EXIT_#{$label_index})\n\n"
-#       $label_index=$label_index+1
-#
-#   elsif command.downcase == "gt" then
-#     output = "// greater than?
-# @SP
-# AM=M-1
-# D=M
-# @SP
-# A=M-1
-# A=M
-# D=A-D
-# @TRUE_#{$label_index}
-# D;JGT
-#
-# (FALSE_#{$label_index})
-# @SP
-# A=M-1
-# M=0
-# @EXIT_#{$label_index}
-# 0;JMP
-#
-# (TRUE_#{$label_index})
-# @SP
-# A=M-1
-# M=-1
-#
-# (EXIT_#{$label_index}) \n\n"
-#       $label_index=$label_index+1
-#
-#     elsif command.downcase == "neg" then
-#       output = "// negate value at SP-1
-# @SP
-# AM=M-1
-# D=-M
-# M=D
-# @SP
-# M=M+1 \n\n"
-#
-#     elsif command.downcase == "and" then
-#       output = "// bitwise and
-# @SP
-# AM=M-1
-# D=M
-# @SP
-# AM=M-1
-# A=M
-# D=A&D
-# @SP
-# A=M
-# M=D
-# @SP
-# M=M+1 \n\n"
-#
-#     elsif command.downcase == "or" then
-#       output = "// bitwise or
-# @SP
-# AM=M-1
-# D=M
-# @SP
-# AM=M-1
-# A=M
-# D=A|D
-# @SP
-# A=M
-# M=D
-# @SP
-# M=M+1 \n\n"
-#
-#     end
-#     return output
   end
 
-  def writePush(command,segment,index)
 
-    infile = File.open(@segment_hash[command + " " + segment], 'r')
-    erb = ERB.new(infile.read)
-    @output.write erb.result(binding)
-
-  end
-
-  # write assembly code that's the tranlsation of a
+  # write assembly code that's the translation of a
   # given command (for C_PUSH/ C_POP)
   # input: command (C_PUSH/C_POP), string (segment), int (index)
   # output: none
   def writePushPop(command,segment,index)
-    output = ""
-    # command is push
-#     if command == "C_PUSH" then
-#       output = "//push #{segment} #{index}
-# @#{index}
-# D=A
-# @SP
-# A=M
-# M=D
-# @SP
-# M=M+1\n\n"
-if command == "push"
-    writePush(command,segment,index)
 
-    # command is pop
-    else
-      output = "//pop #{segment} #{index}
-@#{index}
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M-1\n\n"
+    key = command + " " + segment
+    if @segment_hash.has_key?(key)
+
+      File.open(@segment_hash[key], 'r') do |infile|
+	erb = ERB.new(infile.read)
+	@output.write erb.result(binding)
+
+      end
+
+    else puts "#{command} #{segment} not supported\n\n"
     end
 
-    return output
   end
 
   # close output file
--- project07/Parser.rb
+++ project07/Parser.rb
@@ -20,29 +20,34 @@ class Parser
       if input[input.size-1].chr != "/"
 	input = input + "/"
       end
+
+      # get all vm files from that foler
       files = Dir.glob("#{input}*.vm")
 
       filename = ""
-      i = 0
-      @fileList = Array.new(files.length)
+
+      # count the # of vm files in this folder
+      file_count = 0
+      fileList = Array.new(files.length)
+
+      # for each vm file
       files.each do |file|
 
 	vmFile = File.new(file,"r")
 	filename = File.basename(file,".vm")
 	filename = filename + ".asm"
-	@fileList[i] = vmFile
-	i=i+1
+	fileList[file_count] = vmFile
+	file_count=file_count+1
       end
 
       # create path to write file
       path = Dir.pwd + "/" + input + filename
-      print "Will be writing to ", input, filename, "\n\n"
+      print "Will be writing to #{input}#{filename}\n\n"
 
       # open output file
-      #@vm = File.open(path,"w")
       @code = CodeWriter.new(path)
 
-      @fileList.each do |file|
+      fileList.each do |file|
 	readFile(file)
 	file.close
       end
@@ -54,46 +59,49 @@ class Parser
     elsif File.file?(input)
 
       # Open file & pull the name of file to give the output the same file name
-      @file = File.new(input,"r")
+      file = File.new(input,"r")
       filename = File.basename(input,".vm")
       filename = filename + ".asm"
 
       # pull the directory
       folder= File.dirname(input)
       path = folder + "/" + filename
-      print "Will be writing to ", path, "\n\n"
+      print "Will be writing to #{File.dirname(input)}/#{filename}\n\n"
 
       # open output file
-       @code = CodeWriter.new
+      @code = CodeWriter.new(path)
 
       readFile(file)
       file.close
       @code.close
 
     # maybe the user inputted something wrong?
-
-    else print input, " is not found.  Does it even exist?\n"
+    else puts "#{input} is not found.  Does it even exist?\n"
     end
+
   end
 
   def readFile(file)
 
+    # store all lines as an array
     lines = file.readlines
     lines.each do |line|
-      # remove all comments
+
+      # remove all comments from file
       line = line.gsub(/\/{2}.*/, '')
 
       # split each line on whitespace
       commands = line.split
       if !commands.empty?
-      type = commandType(commands[0])
 
-	if type == "C_PUSH" or type == "C_POP"
+	type = commandType(commands[0])
+
+	if type == "C_PUSH" or type == "C_POP" then
 	  @code.writePushPop(commands[0],commands[1], commands[2])
-
 	else
 	 @code.writeArithmetic(commands[0])
 	end
+
       end
     end
   end
@@ -106,13 +114,17 @@ class Parser
   # output: C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO,
   # 		C_IF, C_FUNCTION, C_RETURN, C_CALL
   def commandType(command)
+
     if command.downcase == "push"
       return "C_PUSH"
+
     elsif command.downcase == "pop"
       return "C_POP"
+
     else
       return "C_ARITHMETIC"
     end
+
   end
 
   # return 1st argument of current commands
