7f763b186b8c97d479eb3145e900be68e029df4d
Shanley Philip <sphilip@mymail.mines.edu>
2011-03-08 20:38:48 -0700

    VM works on BasicLoop

---
+++ project08/CodeWriter.rb
@@ -0,0 +1,135 @@
+require 'erb'
+
+# tranlsation VM commands into Hack assembly code
+class CodeWriter
+$label_index=0
+
+
+  # open output file
+  def initialize(path)
+
+    @output = File.open(path,"w")
+
+    @segment_hash = {
+      "push constant" => "./templates/push-constant.erb",
+      "push local" => "./templates/push-lcl.erb",
+      "push argument" => "./templates/push-arg.erb",
+      "push this" => "./templates/push-this.erb",
+      "push that" => "./templates/push-that.erb",
+      "push temp" => "./templates/push-temp.erb",
+      "push pointer" => "./templates/push-pointer.erb",
+      "push static" => "./templates/push-static.erb",
+      
+      "pop constant" => "./templates/pop-constant.erb",
+      "pop local" => "./templates/pop-lcl.erb",
+      "pop argument" => "./templates/pop-arg.erb",
+      "pop this" => "./templates/pop-this.erb",
+      "pop that" => "./templates/pop-that.erb",
+      "pop temp" => "./templates/pop-temp.erb",
+      "pop pointer" => "./templates/pop-pointer.erb",     
+      "pop static" => "./templates/pop-static.erb"
+    }
+
+    @arith_hash = {
+      "add" => "./templates/add.erb",
+      "sub" => "./templates/sub.erb",
+      "eq" => "./templates/eq.erb",
+      "lt" => "./templates/lt.erb",
+      "gt" => "./templates/gt.erb",
+      "and" => "./templates/and.erb",
+      "or" => "./templates/or.erb"
+    }
+    
+  end
+
+  # inform code writer that translation of new VM file is started
+  # input: string (file name)
+  # output: none
+  def  setFileName(path)
+  end
+
+
+  # write assembly code
+  # input: string (the command)
+  # output: none
+  def writeArithmetic(command)
+
+    if @arith_hash.has_key?(command) then
+   
+      File.open(@arith_hash[command],'r') do |infile|
+	erb = ERB.new(infile.read)
+	@output.write erb.result(binding)
+
+      end
+
+    else puts "#{command} not supported. \n\n"
+    end
+
+  end
+
+
+  # write assembly code that's the translation of a
+  # given command (for C_PUSH/ C_POP)
+  # input: command (C_PUSH/C_POP), string (segment), int (index)
+  # output: none
+  def writePushPop(command,segment,index)
+    
+    key = command + " " + segment
+    if @segment_hash.has_key?(key)
+
+      File.open(@segment_hash[key], 'r') do |infile|
+      erb = ERB.new(infile.read)
+      @output.write erb.result(binding)
+      end
+
+    else puts "#{command} #{segment} not supported\n\n"
+    end
+
+  end
+
+  # write assembly code for label command
+  # input: string
+  def writeLabel(label)
+   @output.print "(#{label})\n\n"
+  end
+  
+  # writes assembly code for goto command
+  # input: string
+  def writeGoto(label)
+    @output.print "@#{label}\n0;JEQ"
+  end
+  
+  # write assembly code for if-goto command
+  # input: string
+  def writeIf(label)
+     File.open("./templates/if-goto.erb", 'r') do |infile|
+      erb = ERB.new(infile.read)
+      @output.write erb.result(binding)
+     end
+  end
+  
+  #write assembly code for call command
+  # input: string (function name), int (number of args)
+  def writeCall(functionName, numArgs)
+  end
+  
+  # write assembly code return command
+  # input: none
+  def writeReturn
+  end
+  
+  # write assembly code for function command
+  # input: string( function name), int ( number of lcls)
+  def writeFunction(functionName, numLcl)
+  end
+  
+  
+  
+  # close output file
+  def close
+    @output.close
+  end
+  
+  
+  
+end
---
+++ project08/Parser.rb
@@ -0,0 +1,159 @@
+# handles the parsing of single .vm fil
+# encapsulates access to input code -> reads, parses & gives access to VM commands & their components
+
+# also removes white space & comments
+
+require 'CodeWriter'
+
+class Parser
+  # Opens inputs file
+  # input: input file
+  # output: none
+
+  def initialize(input)
+    # check if given input is a directory
+
+    if File.directory?(input)
+
+      # it's a directory!
+      # go to that directory & get all .vm files
+      if input[input.size-1].chr != "/"
+	input = input + "/"
+      end
+
+      # get all vm files from that foler
+      files = Dir.glob("#{input}*.vm")
+
+      filename = ""
+
+      # count the # of vm files in this folder
+      file_count = 0
+      fileList = Array.new(files.length)
+
+      # for each vm file
+      files.each do |file|
+
+	vmFile = File.new(file,"r")
+	filename = File.basename(file,".vm")
+	filename = filename + ".asm"
+	fileList[file_count] = vmFile
+	file_count=file_count+1
+      end
+
+      # create path to write file
+      path = Dir.pwd + "/" + input + filename
+      print "Will be writing to #{input}#{filename}\n\n"
+
+      # open output file
+      @code = CodeWriter.new(path)
+
+      fileList.each do |file|
+	readFile(file)
+	file.close
+      end
+
+      @code.close
+
+
+    # it's a file!
+    elsif File.file?(input)
+
+      # Open file & pull the name of file to give the output the same file name
+      file = File.new(input,"r")
+      filename = File.basename(input,".vm")
+      filename = filename + ".asm"
+
+      # pull the directory
+      folder= File.dirname(input)
+      path = folder + "/" + filename
+      print "Will be writing to #{File.dirname(input)}/#{filename}\n\n"
+
+      # open output file
+      @code = CodeWriter.new(path)
+
+      readFile(file)
+      file.close
+      @code.close
+
+    # maybe the user inputted something wrong?
+    else puts "#{input} is not found.  Does it even exist?\n"
+    end
+
+  end
+
+  def readFile(file)
+
+    # store all lines as an array
+    lines = file.readlines
+    lines.each do |line|
+
+      # remove all comments from file
+      line = line.gsub(/\/{2}.*/, '')
+
+      # split each line on whitespace
+      commands = line.split
+      if !commands.empty?
+
+	type = commandType(commands[0])
+
+	if type == "C_PUSH" or type == "C_POP" then
+	  @code.writePushPop(commands[0],commands[1], commands[2])
+	elsif type == "C_LABEL"
+	  @code.writeLabel(commands[1])
+	elsif type == "C_GOTO"
+	  @code.writeGoto(commands[1])
+	elsif type == "C_IF"
+	    @code.writeIf(commands[1])
+	else
+	 @code.writeArithmetic(commands[0])
+	end
+
+      end
+    end
+  end
+
+
+
+  # returns type of current VM command
+  # use C_ARITHMETIC for all arithmetic commands
+  # input: none
+  # output: C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO,
+  # 		C_IF, C_FUNCTION, C_RETURN, C_CALL
+  def commandType(command)
+
+    if command.downcase == "push"
+      return "C_PUSH"
+
+    elsif command.downcase == "pop"
+      return "C_POP"
+
+    elsif command.downcase == "goto"
+      return "C_GOTO"
+      
+    elsif command.downcase == "label"
+      return "C_LABEL"
+    
+    elsif command.downcase == "if-goto"
+      return "C_IF"
+    
+    else
+      return "C_ARITHMETIC"
+    end
+
+  end
+
+  # return 1st argument of current commands
+  # not called if current command = C_RETURN
+  # input: none
+  # output: string
+  def arg1
+  end
+
+  # return 2nd argument of current cmd
+  # only called w/ C_PUSH, C_POP,  C_FUNCTION, C_CALL
+  # input: none
+  # output: int
+  def arg2
+  end
+
+end
