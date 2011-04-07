1cb2af8b3505639f45ac79d907e66ba938953251
Shanley Philip <sphilip@mymail.mines.edu>
2011-04-06 22:49:35 -0600

    Able to create a nested XML file though it is incorrect

--- project10/CompilationEngine.rb
+++ project10/CompilationEngine.rb
@@ -6,61 +6,82 @@ class CompilationEngine
             "var", "let", "do",
 	    "int", "char", "boolean", "void",
             "true", "false", "null",
-            "this",            
+            "this",
             "if", "else", "while", "return"
            ]
 
-@symbol = [ "{", "}",
-            "(", ")",
-            "[", "]",
-            ".", ",", ";",
-            "+", "-", "*", "/",
-            "&", "|",
-            "<", ">",
-            "=", "~"
-          ]
-          
+@symbol = {
+  "{" => "{",
+  "}" => "}",
+  "(" => "(",
+  ")" => ")",
+  "[" => "[",
+  "]" => "]",
+  "." => ".",
+  "," => ",",
+  ";" => ";",
+  "+" => "+",
+  "-" => "-",
+  "*" => "*",
+  "/" => "/",
+  "&" => "&amp;",
+  "|" => "|",
+  "<" => "&lt;",
+  ">" => "&gt;",
+  "=" => "=",
+  "~" => "~"
+          }
+
 #   Creates a new compilation engine with the given input and output. The next routine called must be compileClass().
-  def initialize
-    compileClass
+  def initialize(tokens, xmlFile)
+    @xml = xmlFile
+    compileClass(tokens)
   end
-  
+
 #   Compiles a complete class.
-  def compileClass
+  def compileClass(tokens)
+
+    if tokens[0].downcase == "class"
+      @xml.class{
+	for i in 1...tokens.size
+	  @xml.title(tokens[i])
+	end
+      }
+    end
   end
-  
+
 #   Compiles a static declaration or a field declaration.
   def compileClassVarDec
   end
-  
+
 #   Compiles a complete method, function, or constructor.
   def compileSubroutine
   end
-  
+
 #   Compiles a (possibly empty) parameter list, not including the enclosing “()”.
   def compileParameterList
   end
-  
+
 #   Compiles a var declaration.
   def compileVarDec
   end
-  
+
 #   Compiles a sequence of statements, not including the enclosing “{}”.
   def compileStatements
   end
-  
+
 #   Compiles a do statement.
   def compileDo
   end
-  
+
 #   Compiles a let statement.
   def compileLet
   end
-    
+
   # Compiles a while statement.
   def compileWhile
   end
-    
+
   # Compiles a return statement.
   def compileReturn
   end
@@ -68,15 +89,15 @@ class CompilationEngine
   # Compiles an if statement, possibly with a trailing else clause.
   def compileIf
   end
-  
+
   # compiles expression
   def compileExpression
   end
-  
+
   # compiles a term.  This routine is faced w/ a slight difficulty when trying to decide b/w some of the alternative parsing rules.  Specifically, if the current token is an identifier, the routine must distinguish b/w a variable/array entry & subroutine  call.  A signle lookahead token, which may be one of "[", "(", or "." suffices to distinguish b/w the 3 possibilities.  Any other token is not art of this term & shouldn't be advanced over
   def compileTerm
   end
-  
+
   # compiles a (possibly empty) comma-separated list of expressions
   def compileExpressionList
   end
--- project10/JackTokenizer.rb
+++ project10/JackTokenizer.rb
@@ -4,68 +4,68 @@ require 'CompilationEngine'
 
 # Removes all comments and white space from the input stream and breaks it into Jack-language tokens, as specified by the Jack grammar.
 class JackTokenizer
-  
+
 #   Opens the input file/stream and gets ready to tokenize it.
   def initialize(param)
-    
+
     if File.directory?(param)
       fileList = Dir.glob("#{param}*.jack")
-           
+
       fileList.each do |f|
-	
+
 	newFile = File.basename(f,".jack") + ".win.xml"
 	path = "./" + param + newFile
-		
+
 	outputFile = File.new(path,"w")
-	
-	xml = Builder::XmlMarkup.new(:target => outputFile, :indent => 2 )
+
+	@xml = Builder::XmlMarkup.new(:target => outputFile, :indent => 2 )
 	print "Will be writing to #{param}#{newFile}\n"
-	
+
 	executeTokenization(f)
+	outputFile.close
       end
-      
-      
+
+
     elsif File.file?(param)
-      
+
       newFile = File.basename(f,".jack") + ".win.xml"
       path = "./" + param + newFile
-      outputFile = File.new(path,"w")
-      xml = Builder::XmlMarkup.new(:target => outputFile, :indent => 2 )
-      
+      outputFile = File.new(path,'w')
+      @xml = Builder::XmlMarkup.new(:target => outputFile, :indent => 2 )
+
       print "Will be writing to #{newFile}\n"
-	
+
       executeTokenization(path)
-      
+      outputFile.close
     end
-    
+
   end
-  
-  
-  
+
+
+
   def executeTokenization(file)
 
+    @xml.instruct!
     inputFile = File.open(file,"r")
     print "Reading the file #{file}\n"
-    
-    compilation = CompilationEngine.new
 
     tokens = []
     inputFile.readlines.each do |line|
       # parse single / multiline comments
       line = line.gsub(/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/,'')
-      
+
       if !line.empty? or !line.nil?
-	line = line.split()
-	tokens = tokens + line.split()	
-      end      
+	tokens = tokens + line.split()
+      end
     end
-    
-    puts tokens.inspect
+
+#     puts tokens.inspect
+    compilation = CompilationEngine.new(tokens, @xml)
     inputFile.close
-    
+
   end
 
-  
+
   #   Returns the type of the current token.
   def tokenType
   end
@@ -73,21 +73,21 @@ class JackTokenizer
 #   Returns the keyword which is the current token. Should be called only when tokenType() is KEYWORD.
   def keyWord
   end
-  
+
 #   Returns the character which is the current token. Should be called only when tokenType() is SYMBOL.
   def symbol
   end
-  
+
 #   Returns the identifier which is the current token. Should be called only when tokenType() is IDENTIFIER
   def identifier
   end
-  
+
 #   Returns the integer value of the current token. Should be called only when tokenType() is INT_CONST
   def intVal
   end
-  
+
 #   Returns the string value of the current token, without the double quotes. Should be called only when tokenType() is STRING_CONST.
   def stringVal
   end
-    
+
 end
