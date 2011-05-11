f9baedbed9a1bef4c66a47c9bc4ad9a50f0d9b7a
Shanley Philip <sphilip@mymail.mines.edu>
2011-05-10 19:32:38 -0600

    Added basic error checking since I apparently don't know how to use it, even though I made it

--- project11/compileTest.sh
+++ project11/compileTest.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 WD=`pwd`
-TEST=$WD/Seven/
+# TEST=$WD/Seven/
+# TEST=$WD/ConvertToBin/
+TEST=$WD/$1
 DEF_COMPILE=./../../tools/JackCompiler.sh
 ANALYZE=./JackAnalyzer.rb
 CUSTOM_COMPILE=./JackAnalyzer.rb
 
+if [ $# -eq 0 ]
+then
+echo "Must include dir to compile. Ya gotta compile with your analyzer by yourself!"
+else
 $DEF_COMPILE $TEST
+# $ANALYZE $TEST -- doesn't work
+fi
