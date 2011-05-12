766b3dd4cebe4528518f93d3709f9dd85029d3ac
Shanley Philip <sphilip@mymail.mines.edu>
2011-05-12 10:36:12 -0600

    Added Keyboard to script

--- project12/compile.sh
+++ project12/compile.sh
@@ -8,7 +8,7 @@
 # )
 TEST="Test/"
 
-DONE=("Math" "String" "Sys" "Screen")
+DONE=("Math" "String" "Sys" "Screen" "Keyboard")
 EXT=".jack"
 
 DIR=`echo $HOME`
@@ -17,7 +17,7 @@ WD=`pwd`
 CSCI="/Desktop/CSCI598/cs598/"
 JACK="bash $HOME/Desktop/CSCI598/cs598/tools/JackCompiler.sh"
 
-for index in {0..3}
+for index in {0..4}
 do
 # index=$index-1
     $JACK $WD/"${DONE[$index]}$TEST"
